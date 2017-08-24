525e9be9a87d36a37c755b041495aed8ffebad17
Jakub Dundalek <dundalek@gmail.com>
2017-08-24 17:05:02 +0200

    TDD is useful sometimes

---
+++ test/parse.markdown.test.js
@@ -0,0 +1,30 @@
+var parse = require('../parse.markdown');
+
+it('parses headings', () => {
+  expect(parse("# a\n## b")).toEqual([
+    {"depth": 1, "line": 0, "name": "a"},
+    {"depth": 2, "line": 1, "name": "b"}
+  ]);
+});
+
+it('parses lists', () => {
+  expect(parse("- a\n  - a 1\n  - a 2\n- b")).toEqual([
+    {"depth": 1, "line": 0, "name": "a"},
+    {"depth": 2, "line": 1, "name": "a 1"},
+    {"depth": 2, "line": 2, "name": "a 2"},
+    {"depth": 1, "line": 3, "name": "b"},
+  ]);
+});
+
+it('parses asterisk lists', () => {
+  expect(parse("* a\n  * a 1\n  * a 2")).toEqual([
+    {"depth": 1, "line": 0, "name": "a"},
+    {"depth": 2, "line": 1, "name": "a 1"},
+    {"depth": 2, "line": 2, "name": "a 2"},
+    {"depth": 1, "line": 3, "name": "b"},
+  ]);
+});
+
+it('does not parse lists when options is passed', () => {
+  expect(parse("* a\n  * a 1\n  * a 2", { lists: false })).toEqual([]);
+});
