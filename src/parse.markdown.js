908126256a25ae8705714e1b94097cdd6515225c
Jakub Dundalek <dundalek@gmail.com>
2018-04-24 23:33:16 +0200

    Extract links

--- src/parse.markdown.js
+++ src/parse.markdown.js
@@ -1,6 +1,36 @@
+
+function extractLinks(tokens) {
+  var result = [];
+  var href;
+  var parts;
+  for (var i = 0; i < tokens.length; i += 1) {
+    var token = tokens[i];
+    switch (token.type) {
+      case "link_open":
+        href = token.href;
+        parts = [];
+        break;
+      case "text":
+        if (parts) {
+          parts.push(token.content)
+        }
+        break;
+      case "link_close":
+        result.push({
+          href: href,
+          name: parts.join('')
+        });
+        parts = null;
+        break;
+    }
+  }
+  return result;
+}
+
 module.exports = function parseMarkdown(text, options) {
   options = options || {};
   parseLists = options.lists !== false;
+  parseLinks = options.links !== false;
 
   var Remarkable = require('remarkable');
   var md = new Remarkable();
@@ -8,7 +38,7 @@ module.exports = function parseMarkdown(text, options) {
     'deflist'
   ]);
 
-  var tokens = md.parse(text, {})
+  var tokens = md.parse(text, {});
   var headings = [];
   var depth = 0;
   for (var i = 0; i < tokens.length; i += 1) {
@@ -20,6 +50,14 @@ module.exports = function parseMarkdown(text, options) {
         name: tokens[i+1].content || ''
       });
       i += 1;
+    } else if (tokens[i].type === 'inline') {
+      if (parseLinks) {
+        headings = headings.concat(extractLinks(tokens[i].children).map(function(x) {
+          x.depth = depth + 1;
+          x.line = tokens[i].lines[0];
+          return x;
+        }));
+      }
     } else if (parseLists) {
       switch (tokens[i].type) {
         case 'bullet_list_open':
